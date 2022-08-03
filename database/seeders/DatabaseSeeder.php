fc755f556fc14863978c538b38cae8132b7956ee
J Arias <joelarias@unicesar.edu.co>
2022-08-03 11:07:35 -0500

    adding subsidiary table, adding factories to create fake data

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,8 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Client;
+use App\Models\Subsidiary;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -22,5 +24,7 @@ class DatabaseSeeder extends Seeder
         // ]);
 
         $this->call(UserRolePermissionSeeder::class);
+        Subsidiary::factory(5)->create();
+        Client::factory(30)->create();
     }
 }
